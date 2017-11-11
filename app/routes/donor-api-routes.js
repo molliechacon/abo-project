81810e5c74b6ef4fd44b7c439bf4336fdd05afaf
mollie chacon <mollie.mosley@gmail.com>
2017-11-11 11:48:52 -0700

    final update

--- app/routes/donor-api-routes.js
+++ app/routes/donor-api-routes.js
@@ -1,34 +1,3 @@
-// NEEDS UPDATING!!!
-
-// UI ENDPOINTS???
-//      /                                --> login page
-//      /donor/:donor_id                 --> donation history
-//      /donor/:donor_id/profile         --> display profile info
-//      /inst/:inst_id                   --> current need and received this mo
-//      /inst/:inst_id/profile           --> display profile info
-
-
-// API ENDPOINTS???
-//      /api/donors             --> POST add a new donor profile 
-
-//      /api/donors/:donor_id   --> GET donation history
-//                              --> PUT update profile info
-//                              --> PUT log a donation (or combine with above)
-//                              --> DELETE account (move to inactive or mark as inactive)
-
-//      /api/inst               --> POST add a new inst profile
-
-//      /api/inst/:inst_id      --> GET current need
-//                              --> PUT update current need
-//                              --> GET received this month (two gets on the same endpt??)
-//                              --> PUT update inst info
-//                              --> DELETE account??
-
-
-
-
-
-
 // Requiring our models
 var Donor = require("../models/donors");
 
