11b0d21713db9eca8f9feee6563c494a57ec7e03
Partha Monge <parthamonge@gmail.com>
2017-11-11 08:35:36 -0700

    Revert "Trying to get login redirect to work"

--- app/public/js/donor.js
+++ app/public/js/donor.js
@@ -1,123 +1,124 @@
-function createDonorObj() {
+function createDonorObj () {
 
-  let dataObj = {};
+    let dataObj = {};   
 
-  if ($("#input-nickname").val()) {
-    dataObj.nick_name = $("#input-nickname").val().trim();
-  } else {
-    dataObj.nick_name = $("#current-name").text();
-  };
+        if ($("#input-nickname").val()) {
+            dataObj.nick_name = $("#input-nickname").val().trim();
+        } else {
+            dataObj.nick_name = $("#current-name").text();
+        };
 
-  if ($("#input-email").val()) {
-    dataObj.email = $("#input-email").val().trim();
-  } else {
-    dataObj.email = $("#current-email").text();
-  };
+        if ($("#input-email").val()) {
+            dataObj.email = $("#input-email").val().trim();
+        } else {
+            dataObj.email = $("#current-email").text();
+        };
 
-  if ($("#input-address").val()) {
-    dataObj.street_address = $("#input-address").val().trim();
-  } else {
-    dataObj.street_address = $("#current-address").text();
-  };
+        if ($("#input-address").val()) {
+            dataObj.street_address = $("#input-address").val().trim();
+        } else {
+            dataObj.street_address = $("#current-address").text();
+        };
 
-  if ($("#input-zip").val()) {
-    dataObj.zip_code = $("#input-zip").val().trim();
-  } else {
-    dataObj.zip_code = $("#current-zip").text();
-  };
+        if ($("#input-zip").val()) {
+            dataObj.zip_code = $("#input-zip").val().trim();
+        } else {
+            dataObj.zip_code = $("#current-zip").text();
+        };
 
-  if ($("#input-type").val()) {
-    dataObj.blood_type = $("#input-type").val().trim();
-  } else {
-    dataObj.blood_type = $("#current-type").text();
-  };
+        if ($("#input-type").val()) {
+            dataObj.blood_type = $("#input-type").val().trim();
+        } else {
+           dataObj.blood_type = $("#current-type").text();
+        };
 
-  if ($("#input-phone").val()) {
-    dataObj.phone = $("#input-phone").val().trim();
-  } else {
-    dataObj.phone = $("#current-phone").text();
-  };
+        if ($("#input-phone").val()) {
+            dataObj.phone = $("#input-phone").val().trim();
+        } else {
+            dataObj.phone = $("#current-phone").text();
+        };
 
-  if ($("input[name=inlineRadioOptions1]:checked").val()) {
-    dataObj.accept_email = $("input[name=inlineRadioOptions1]:checked").val();
-  } else {
-    dataObj.accept_email = $("#current-accept-email").text();
-  };
+        if ($("input[name=inlineRadioOptions1]:checked").val()) {
+            dataObj.accept_email = $("input[name=inlineRadioOptions1]:checked").val();
+        } else {
+            dataObj.accept_email = $("#current-accept-email").text();
+        };
 
-  if ($("input[name=inlineRadioOptions2]:checked").val()) {
-    dataObj.accept_sms = $("input[name=inlineRadioOptions2]:checked").val();
-  } else {
-    dataObj.accept_sms = $("#current-accept-sms").text();
-  };
+        if ($("input[name=inlineRadioOptions2]:checked").val()) {
+            dataObj.accept_sms = $("input[name=inlineRadioOptions2]:checked").val();
+        } else {
+            dataObj.accept_sms = $("#current-accept-sms").text();
+        };
 
-  if ($("#input-donation-date").val()) {
-    dataObj.donation_date = $("#input-donation-date").val().trim();
-  } else {
-    dataObj.donation_date = $("#current-donation-date").text();
-  };
+        if ($("#input-donation-date").val()) {
+            dataObj.donation_date = $("#input-donation-date").val().trim();
+        } else {
+            dataObj.donation_date = $("#current-donation-date").text();
+        };
+        
+        if ($("#input-donation-location").val()) {
+            dataObj.institution_name = $("#input-donation-location").val().trim();
+        } else {
+            dataObj.institution_name = $("#current-donation-location").text();
+        };
 
-  if ($("#input-donation-location").val()) {
-    dataObj.institution_name = $("#input-donation-location").val().trim();
-  } else {
-    dataObj.institution_name = $("#current-donation-location").text();
-  };
-
-  return dataObj;
+        return dataObj;
 }
 
+    
+    $("#submit-donation-log").click(function(event) {
+        event.preventDefault();
 
-$("#submit-donation-log").click(function (event) {
-  event.preventDefault();
+        donorUpdate(createDonorObj(), function() {
+            $("#donation-log-confirm").html("<br>" + "Your donation was successfully logged! Please close this window."); 
+            
+            $("#input-donation-date").val("");
+            $("#input-donation-location").val("");
+        });
+    });
 
-  donorUpdate(createDonorObj(), function () {
-    $("#donation-log-confirm").html("<br>" + "Your donation was successfully logged! Please close this window.");
+    $("#submit-donor-profile").click(function(event) {
+        event.preventDefault();
 
-    $("#input-donation-date").val("");
-    $("#input-donation-location").val("");
-  });
-});
+        donorUpdate(createDonorObj(), function() {
+            $("#donor-profile-update-confirm").html("<br>" + "Your profile was successfully updated! Please close this window."); 
+            
+            $("#input-nickname").val("");
+            $("#input-email").val("");
+            $("#input-address").val("");
+            $("#input-zip").val("");
+            $("#input-type").val("");
+            $("#input-phone").val("");
+            $("input[name=inlineRadioOptions1]:checked").val("");
+            $("input[name=inlineRadioOptions2]:checked").val("");
+        });
+    });
 
-$("#submit-donor-profile").click(function (event) {
-  event.preventDefault();
+    // $("#find-donation-center").click(function(event) {
+        
+    // });
 
-  donorUpdate(createDonorObj(), function () {
-    $("#donor-profile-update-confirm").html("<br>" + "Your profile was successfully updated! Please close this window.");
+    $("#close-donation-modal").on("click", function(event) {
+        $("#donation-log-confirm").text("");
+    });
 
-    $("#input-nickname").val("");
-    $("#input-email").val("");
-    $("#input-address").val("");
-    $("#input-zip").val("");
-    $("#input-type").val("");
-    $("#input-phone").val("");
-    $("input[name=inlineRadioOptions1]:checked").val("");
-    $("input[name=inlineRadioOptions2]:checked").val("");
-  });
-});
+    $("#close-profile-modal").on("click", function(event) {
+        $("#donor-profile-update-confirm").text("");
+    });
 
-// $("#find-donation-center").click(function(event) {
-
-// });
-
-$("#close-donation-modal").on("click", function (event) {
-  $("#donation-log-confirm").text("");
-});
-
-$("#close-profile-modal").on("click", function (event) {
-  $("#donor-profile-update-confirm").text("");
-});
-
-function donorUpdate(dataObj, cb) {
-  // console.log(dataObj);
-  $.ajax({
-    method: "PUT",
-    url: "/donors/" + $("#nick-name").text(),
-    data: dataObj
-  }).done(function (result) {
-    console.log("Successful update");
-  }).fail(function (err) {
-    console.log(err);
-  }).always(function (result) {
-    console.log(".always is working");
-    cb();
-  });
-}
+    function donorUpdate(dataObj, cb) {
+        // console.log(dataObj);
+                $.ajax({
+                    method: "PUT",
+                    url: "/donors/" + $("#nick-name").text(),
+                    data: dataObj                    
+                }).done(function(result) {
+                    // err?
+                    console.log("Successful update");
+                }).fail(function(err) {
+                    console.log(err);
+                }).always(function(result) {
+                    console.log(".always is working");
+                    cb();
+                });
+    }
--- app/routes/donor-api-routes.js
+++ app/routes/donor-api-routes.js
@@ -1,88 +1,39 @@
-// NEEDS UPDATING!!!
-
-// UI ENDPOINTS???
-//      /                                --> login page
-//      /donor/:donor_id                 --> donation history
-//      /donor/:donor_id/profile         --> display profile info
-//      /inst/:inst_id                   --> current need and received this mo
-//      /inst/:inst_id/profile           --> display profile info
-
-
-// API ENDPOINTS???
-//      /api/donors             --> POST add a new donor profile 
-
-//      /api/donors/:donor_id   --> GET donation history
-//                              --> PUT update profile info
-//                              --> PUT log a donation (or combine with above)
-//                              --> DELETE account (move to inactive or mark as inactive)
-
-//      /api/inst               --> POST add a new inst profile
-
-//      /api/inst/:inst_id      --> GET current need
-//                              --> PUT update current need
-//                              --> GET received this month (two gets on the same endpt??)
-//                              --> PUT update inst info
-//                              --> DELETE account??
-
-
-
-
-
-
 // Requiring our models
 var Donor = require("../models/donors");
 
-module.exports = function (app) {
-  let users = [
-    { nick_name: 'Mollie', type: 'Donor' },
-    { nick_name: 'Partha', type: 'Donor' },
-    { nick_name: 'Sam', type: 'Donor' },
-    { nick_name: 'SwedishAdmin', type: 'Inst', inst_name: 'Swedish' },
-    { nick_name: 'NationalJewishAdmin', type: 'Inst', inst_name: 'NationalJewish' },
-    { nick_name: 'PorterAdmin', type: 'Inst', inst_name: 'Porter' }
-  ];
+module.exports = function(app) {
 
-  app.get("/donors/:nick_name", function (req, res) {
-    Donor.findOne({
-      where: {
-        nick_name: req.params.nick_name
-      }
-    }).then(function (aDonor) {
-      console.log(aDonor);
-      res.render("donor", {
-        donor: aDonor
-      });
+    app.get("/donors/:nick_name", function(req, res) {
+        Donor.findOne({
+            where: {
+                nick_name: req.params.nick_name
+            }
+        }).then(function(aDonor) {
+                res.render("donor", { donor: aDonor })
+            })
+    });    
+
+
+    app.put("/donors/:nick_name", function(req, res) {
+        Donor.update({
+            nick_name: req.body.nick_name,
+            email: req.body.email,
+            accept_email: req.body.accept_email,
+            street_address: req.body.street_address,
+            zip_code: req.body.zip_code,
+            blood_type: req.body.blood_type,
+            phone: req.body.phone,
+            accept_sms: req.body.accept_sms,
+            donation_date: req.body.donation_date,
+            institution_name: req.body.institution_name
+        }, {
+            where: {
+                nick_name: req.params.nick_name
+            }
+        }).then(function(update) {
+            // error catch??
+            res.end("");
+            console.log("Your update is complete")
+        });
     });
-  });
-
-  app.put("/donors/:nick_name", function (req, res) {
-    Donor.update({
-      nick_name: req.body.nick_name,
-      email: req.body.email,
-      accept_email: req.body.accept_email,
-      street_address: req.body.street_address,
-      zip_code: req.body.zip_code,
-      blood_type: req.body.blood_type,
-      phone: req.body.phone,
-      accept_sms: req.body.accept_sms,
-      donation_date: req.body.donation_date,
-      institution_name: req.body.institution_name
-    }, {
-      where: {
-        nick_name: req.params.nick_name
-      }
-    }).then(function (update) {
-      // error catch??
-      res.end("");
-      console.log("Your donation was logged!")
-    });
-  });
-
-  // process the login form
-  // app.post('/login', do all our passport stuff here);
-  // process the signup form
-  app.post('/', function(req, res){
-    console.log("Reached Login Route");
-    res.redirect('/donors/Partha');
-  });
-};
+}
