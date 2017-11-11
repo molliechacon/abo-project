0db66c4ebabc301882647c21e38ee30f84d2c746
pdmonge <parthamonge@gmail.com>
2017-11-11 11:15:59 -0700

    For Presentation-- maybe

--- app/public/js/login.js
+++ app/public/js/login.js
@@ -1,3 +1,26 @@
-$("#login-button-submit").click(function(event) {
+var users = [
+  { nick_name: 'Mollie', type: 'Donor' },
+  { nick_name: 'Partha', type: 'Donor' },
+  { nick_name: 'Sam', type: 'Donor' },
+  { nick_name: 'SwedishAdmin', type: 'Inst', inst_name: 'Swedish' },
+  { nick_name: 'NationalJewishAdmin', type: 'Inst', inst_name: 'NationalJewish' },
+  { nick_name: 'PorterAdmin', type: 'Inst', inst_name: 'Porter' }
+];
 
+$("#login-button-submit").click(function(event) {
+  event.preventDefault();
+  let userName = $('#login-input-nick-name').val().trim();
+  if (!userName) return;
+  var thisUser;
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].nick_name === userName) {
+      thisUser = users[i];
+    }
+  }
+  console.log(thisUser.inst_name);
+  if (thisUser.type === 'Donor') {
+    window.location.assign(`/donors/${userName}`);
+  } else if (thisUser.type === 'Inst') {
+    window.location.assign(`/insts/${thisUser.inst_name}`);    
+  }
 });
